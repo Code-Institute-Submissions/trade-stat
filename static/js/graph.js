f6f581d9ee192165f05c72ecf196bf0c810c44dd
Jevgeni Glazunov <jevgeni.glazunov@gmail.com>
2019-01-11 13:35:35 +0000

    delete selector menu

--- static/js/graph.js
+++ static/js/graph.js
@@ -9,27 +9,18 @@ function makeGraphs(error, tradeData) {
     
     //string to number
     tradeData.forEach(function(d){
+        d.Date = ["Date(UTC)"];
         d.Price = parseFloat(d.Price);
         d.Amount = parseFloat(d.Amount);
         d.Total = parseFloat(d.Total);
         d.Fee = parseFloat(d.Fee);
     });
-    show_market_selector(ndx);
     show_trading_pairs(ndx);
     show_buysell_orders(ndx);
     show_trading_volume(ndx);
     show_gainloss_timeline(ndx);
     
     dc.renderAll();
-}
-
-function show_market_selector(ndx) {
-    var marketDim = ndx.dimension(dc.pluck("Market"));
-    var selectorMenu = marketDim.group();
-    
-    dc.selectMenu("#market-selector")
-        .dimension(marketDim)
-        .group(selectorMenu);
 }
  
 //Pie chart with amount of trades on pairs
@@ -73,6 +64,7 @@ function show_buysell_orders(ndx) {
 //Bar chart with trading volume per pair
 function show_trading_volume(ndx) {
     var marketDim = ndx.dimension(dc.pluck("Market"));
+    
     var tradingVolume = marketDim.group().reduce(
         function (p, v) {
             p.count++;
@@ -111,9 +103,55 @@ function show_trading_volume(ndx) {
 }
 
 function show_gainloss_timeline(ndx) {
-    var dateDim = ndx.dimension(dc.pluck("Date"));
-    var gainLossPeriod =dateDim.group();
+    var dateDimension = ndx.dimension(dc.pluck("Date(UTC)"));
+    var monthlyMoveGroup = dateDimension.group().reduce(
+        function (p, v) {
+            p.count++;
+            p.total += v.buy;
+            return p;
+        },
+        function (p, v) {
+            p.count--;
+            p.total -= v.buy;
+            return p;
+        },
+        function () {
+            return { count:0, total: 0};
+        }
+    );
     
-    
-    
+    dc.lineChart("#gain-loss-period")
+        .width(1000)
+        .height(500)
+        .dimension(dateDimension)
+        .group(monthlyMoveGroup)
+        .valueAccessor(function (d) {
+            if (d.value.count == 0) {
+                return 0;
+            } else {
+                return d.value.total;
+            }
+        })
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .elasticY(true);
 }
+
+function diff(value) {
+	let buy;
+	if(value.Type == "BUY") {
+		let buy = true;
+	} else {
+		let buy = false;
+	}
+	if(buy) {
+		value.Total++;
+	} else {
+		value.Total--;
+	}
+	
+	return buy;
+	console.log(buy);
+}
+
